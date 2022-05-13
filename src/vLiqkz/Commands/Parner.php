7ebacccd3d4e44f72527873d7058b99be29a6a3f
vNiqkz <97852466+vNiqkz@users.noreply.github.com>
2022-05-12 20:32:28 -0400

    update

--- src/vLiqkz/Commands/Parner.php
+++ src/vLiqkz/Commands/Parner.php
@@ -1,34 +1,47 @@
 <?
 namespace vLiqkz\Commands;
 use pocketmine\command\{Command, CommandSender};
-class CreatePartner extends Command{
+class AdminPartner extends Command{
     public function __construct(){
-		parent::__construct("CreatePartner", "Con este comando puedes crear un partner para el menu", "/createPartner [String: Name]", ["createPartner", "cpartner"]);
+		parent::__construct("AdminPartner", "Con este comando puedes crear un partner para el menu", "/createPartner [String: Name]", ["createPartner", "cpartner"]);
 	}
-    //hola tobby xd
     public function execute(CommandSender $sender, string $commandLabel, array $args): void{
         if(emply($args)){
-            $sender->sendMessage('necesitas ponerle un nombre al partner');
+            $sender->sendMessage('');
             return;
         }
         $func = $args[0];
-        switch ($func) {
+        switch (strtolower($func)) {
             case 'edit':
-                    if(!$args[1]){
-                        $sender->sendMessage('necesitas el nombre del partner para editarlo');
-                        return;
-                    }
+                if(!$args[1]){
+                    $sender->sendMessage('necesitas el nombre del partner para editarlo');
+                    return;
+                }
+                
+                $pname = '';
+                for($i = 1; $i < count($args); $i++){
+                    $pname = $pname + $args[$i];
+                }
+                //code
                 break;
             case 'create':
                 if(!$args[1]){
                     $sender->sendMessage('necesitas ponerle un nombre al partner');
                 }
-                $pname = '';
-                for($i = 1; $i < count($args); $i++){
-                    $pname = $pname + $args[$i];
-                }
-                Main::CreatePartner($pname);
-                break;
+            $pname = '';
+            for($i = 1; $i < count($args); $i++){
+                $pname = $pname + $args[$i];
+            }
+            Main::CreatePartner($pname);
+            break;
+            case 'remove':
+            if(!$args[1]){
+            $pname = '';
+            for($i = 1; $i < count($args); $i++){
+                $pname = $pname + $args[$i];
+            }
+            Main::DeletePartner($pname);
+            break;
             default:
             $sender->sendMessage('funcion no encontrada');
                 break;
